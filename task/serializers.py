eef5b32279abc34662c287a01de9579166924282
Amir Savand <amir@savandbros.com>
2019-04-14 10:51:48 +0300

    Use ID for TaskSerializer.project

--- task/serializers.py
+++ task/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
 
-from project.serializers import ProjectSerializer
 from task.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    project = ProjectSerializer()
-
     class Meta:
         model = Task
         fields = '__all__'
