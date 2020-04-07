f7a17af09ba19a64244488030cd39692fbec94fc
Amir Savand <amir@savandbros.com>
2020-04-07 21:22:16 +0300

    Move chat to stabber

--- stabber/models.py
+++ stabber/models.py
@@ -137,3 +137,20 @@ class LabelObject(models.Model):
 
     class Meta:
         unique_together = (("label", "kind", "to"),)
+
+
+class Chat(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def organization(self):
+        return self.project.organization
+
+    def __str__(self):
+        return "{user}: {content}".format(user=self.user, content=self.content)
+
+    class Meta:
+        ordering = ("-id",)
